0cb16615c3ca4b9fbc5d689f4e8d05d18fed9d75
ronald van der kooij <ronald@sharpsolutions.nl>
2014-09-09 10:14:22 +0200

    implemented the registerassingleton functionality

--- ioc.js
+++ ioc.js
@@ -1,12 +1,23 @@
-function applyToConstructor(constructor, argArray) {
-    var args = [null].concat(argArray);
-    var factoryFunction = constructor.bind.apply(constructor, args);
-    return new factoryFunction();
-}
-
 var ioc = function(){
-
+	
+	var _this = this;
 	var registry = {};
+	var singletonRegistry = {};
+
+	this.registerAsSingleton = function(name, subject){
+
+		if(singletonRegistry[name]){
+			throw new Error(name +' is already registered!');
+		}
+		
+		if( typeof(subject) === 'function' ){
+			singletonRegistry[name] = applyContructorFunction(subject);
+		}
+		else{
+			singletonRegistry[name] = subject;
+		}
+		
+	};
 
 	this.register = function(name, subject){
 		
@@ -18,27 +29,15 @@ var ioc = function(){
 	};
 
 	this.resolve = function(name){
-		var result = registry[name];
+		var result = registry[name] || singletonRegistry[name];
 
 		if(!result){
 			throw new Error('Cannot resolve anything with the name: ' + name);
 		}
 
 		if( typeof(result) === 'function' ){
-			
-			var arguments = [ ];
-			
-			var args = result.toString().match(/\((.*)\)/)[1].split(",");
-    
-    		for (var i = 0; i < args.length; i++) {
-    			
-    			if(args[i] !== ''){
-    				
-    				arguments.push(this.resolve(args[i].trim()));
-    			}
-    		};
 
-			return applyToConstructor(result, arguments);    		        	
+			return applyContructorFunction(result);    		        	
 		}
 		
 		return result;
@@ -47,6 +46,28 @@ var ioc = function(){
 	this.reset = function(){
 		registry = {};
 	};
+
+	function getArgumentsFromFunction(fn){
+		return fn.toString().match(/\((.*)\)/)[1].split(",");
+	}
+
+	function applyContructorFunction(constructorFunction) {
+	    var arguments = [ ];
+				
+		var args = getArgumentsFromFunction(constructorFunction);
+
+		for (var i = 0; i < args.length; i++) {
+	    			
+			if(args[i] !== ''){
+				
+				arguments.push(_this.resolve(args[i].trim()));
+			}
+		};
+
+	    var argsArray = [null].concat(arguments);
+	    var factoryFunction = constructorFunction.bind.apply(constructorFunction, argsArray);
+	    return new factoryFunction();
+	}
 };
 
 module.exports = new ioc();
--- tests.js
+++ tests.js
@@ -3,39 +3,45 @@ var ioc = require('./index');
 
 describe('Ioc tests: ', function(){
 	
+	// ---------------------------
+	// General tests
+	// ---------------------------
 	it('It should be able to be resolve', function(){
+		// arrange
 		ioc.reset();
 		var input = { };
 		ioc.register('test', input)
+		
+		// act
 		var output = ioc.resolve('test');
-		assert.equal(input, output);
-	});
-
-	it('It should be able to be resolve2', function(){
-		ioc.reset();
-		var input = { };
-		ioc.register('test', input)
-		var output = ioc.resolve('test');
+		
+		// assert
 		assert.equal(input, output);
 	});
 
 	it('It should throw when registering with an existing name', function(){
+		// arrange
 		ioc.reset();
 		ioc.register('test', { })
+		
+		// act & assert
 		assert.throws( function(){
 			ioc.register('test', {});
 		}, Error );
 	});
 
 	it('It should throw when it cannot resolve', function(){
+		// arrange
 		ioc.reset();
 		
+		// act & assert
 		assert.throws( function(){
 			ioc.resolve('unregisteredname', {});
 		}, Error );
 	});
 
-	it('It should new an object when possible', function(){
+	it('It should new every object which is registered normally', function(){
+		// arrange
 		ioc.reset();
 		
 		var myfunction = function(){
@@ -45,12 +51,16 @@ describe('Ioc tests: ', function(){
 
 		ioc.register('instance', myfunction);
 
+		// act & assert
 		var instance = ioc.resolve('instance');
-
 		assert.equal(instance.property, 'myproperty');
+		instance.property = 'mypropertychanged';
+		var instance2 = ioc.resolve('instance');
+		assert.equal(instance2.property, 'myproperty');
 	});
 
 	it('It should also inject dependencies into resolved objects', function(){
+		// arrange
 		ioc.reset();
 		
 		var mysubdependency1 = function(){
@@ -70,11 +80,62 @@ describe('Ioc tests: ', function(){
 		ioc.register('mysubdependency2', mysubdependency2);
 		ioc.register('mydependency', mydependency);
 
+		// act
 		var resolved = ioc.resolve('mydependency');
 
+		// assert
 		assert.equal(resolved.name, 'mydependency');
 		assert.equal(resolved.sub1.name, 'mysubdependency1');
 		assert.equal(resolved.sub2.name, 'mysubdependency2');
+	});
+	
+	// ---------------------------
+	// Singleton tests
+	// ---------------------------
+	it('It should return a singleton when a function is registerd as singleton', function(){
+		// arrange
+		ioc.reset();
+		
+		var myfunction = function(){
+			
+			this.property = 'myproperty';
+		}
+
+		ioc.registerAsSingleton('instance', myfunction);
+
+		// act % assert
+		var instance = ioc.resolve('instance');
+		instance.property = 'mypropertychanged';
+		var instance2 = ioc.resolve('instance');
+		assert.equal(instance2.property, 'mypropertychanged');
+	});
+
+	it('It should return a singleton when an object is registerd as singleton', function(){
+		// arrange
+		ioc.reset();
+		
+		var myObject = {
+			property: 'myproperty'
+		}
+
+		ioc.registerAsSingleton('object', myObject);
+
+		// act % assert
+		var resolvedObject = ioc.resolve('object');
+		resolvedObject.property = 'mypropertychanged';
+		var resolvedObject2 = ioc.resolve('object');
+		assert.equal(resolvedObject2.property, 'mypropertychanged');
+	});
+
+	it('It should throw when registering a singleton with an existing name', function(){
+		// arrange
+		ioc.reset();
+		ioc.registerAsSingleton('test', { })
+		
+		// act & assert
+		assert.throws( function(){
+			ioc.registerAsSingleton('test', {});
+		}, Error );
 	});
 });
 
